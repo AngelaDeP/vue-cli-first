a07db9552d2fda50cd8b0a956b50d2669788d7fc
Angela De Pasquale <angeladepasquale_@libero.it>
2022-02-18 17:22:43 +0100

    implementato primo componente

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NewTitle></NewTitle>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import NewTitle from "./components/NewTitle.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    NewTitle,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: "NewTitle"
+};
+</script>
+
+<style scoped>
+
+    h1 {
+        color: rgba(218, 10, 10);
+    }
+
+</style>
